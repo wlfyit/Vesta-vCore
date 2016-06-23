7b5ea89bc4fbdcd12a5b4fd18545dac6208260be
Wolfy Meier <wolfy@wlfy.it>
2016-06-23 15:56:29 -0700

    added telegram module

---
+++ controllers/telegram.js
@@ -0,0 +1,11 @@
+'use strict';
+
+var TelegramBot = require('node-telegram-bot-api');
+
+module.exports = function (redis, ks, logger) {
+  var redisPrefix       = 'telegram:';
+
+  var TelegramController = {};
+
+  return TelegramController;
+};
